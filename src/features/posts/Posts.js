58f2daff7b67e1398307080c899b277fd7669dc7
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-06-08 16:18:04 +0530

    Added tailwindcss

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -4,7 +4,7 @@ export const Posts = () => {
   useDocumentTitle("Posts");
   return (
     <div>
-      <h2>Posts</h2>
+      <h2 className="text-4xl text-blue-500 text-center font-bold">Posts</h2>
     </div>
   );
 };
