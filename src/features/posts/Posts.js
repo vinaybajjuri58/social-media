38eeb313d9b7e86267016304f05bb2f93f5836c2
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-06-29 13:24:50 +0530

    Added axios package

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,11 +1,17 @@
 import { useDocumentTitle } from "../../customHooks";
 import { Post } from "../../Components/Post";
+import axios from "axios";
+import { useEffect } from "react";
 export const Posts = () => {
   useDocumentTitle("Posts");
+  // useEffect(()=>{
+  //   (async()=>{
+  //     axios.get("")
+  //   })()
+  // },[])
   return (
     <div>
       <ul className="list-none">
-        <h2>Posts</h2>
         <Post />
       </ul>
     </div>
