16dcb918c7966ec652d30ffb0cec581a4816b311
vinaybajjuri58 <vinaybajjuri58@gmail.com>
2021-06-30 10:30:58 +0530

    Refactor:moved signup login files to authSlice

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import { Posts } from "./features/posts/Posts";
 import { Notifications } from "./features/notifications/Notifications";
-import { Login, SignUp } from "./features/userData";
+import { Login, SignUp } from "./features/authData";
 import { Routes, Route } from "react-router-dom";
 import { Profile } from "./features/Profile/Profile";
 import { PrivateRoute } from "./PrivateRoute";
@@ -9,9 +9,9 @@ export const RoutesComponent = () => {
     <Routes>
       <PrivateRoute path="/notifications" element={<Notifications />} />
       <PrivateRoute path="/posts" element={<Posts />} />
+      <PrivateRoute path="/profile" element={<Profile />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <PrivateRoute path="/profile" element={<Profile />} />
     </Routes>
   );
 };
